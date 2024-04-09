55a076059fc4054f8231fb91e92a04fe65430051
Connor Smith <cosm3320@colorado.edu>
2024-04-08 20:17:00 -0600

    Reverted database to original, simple version

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -4,3 +4,27 @@ CREATE TABLE IF NOT EXISTS users (
  username VARCHAR(60) PRIMARY KEY,
  password VARCHAR(60) NOT NULL
 );
+
+DROP TABLE IF EXISTS majors;
+CREATE TABLE  majors(
+    major_name VARCHAR(50) PRIMARY KEY,
+    major_description CHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS projects;
+CREATE TABLE  projects(
+    project_name VARCHAR(50) PRIMARY KEY,
+    project_description CHAR(500) NOT NULL
+);
+
+DROP TABLE IF EXISTS user_projects;
+CREATE TABLE user_projects (
+  username VARCHAR(50) NOT NULL REFERENCES users(username),
+  project_name VARCHAR(50) NOT NULL REFERENCES projects(project_name)
+);
+
+DROP TABLE IF EXISTS user_majors;
+CREATE TABLE user_majors(
+  username VARCHAR(50) NOT NULL REFERENCES users(username),
+  major_name VARCHAR(50) NOT NULL REFERENCES majors(major_name)
+);
