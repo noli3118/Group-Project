bfbedc03121778c0f98e63ea31200c0f2f371975
Noah Liska <noli3118@colorado.edu>
2024-04-18 16:47:16 -0600

    Added drop statements to get rid of existing dbs

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,30 +1,23 @@
--- DROP TABLE IF EXISTS group_members;
--- DROP TABLE IF EXISTS group_messages;
--- DROP TABLE IF EXISTS project_likes;
--- DROP TABLE IF EXISTS messages;
--- DROP TABLE IF EXISTS user_category;
--- DROP TABLE IF EXISTS user_majors;
--- DROP TABLE IF EXISTS user_projects;
--- DROP TABLE IF EXISTS projects;
--- DROP TABLE IF EXISTS majors;
--- DROP TABLE IF EXISTS users;
--- DROP TABLE IF EXISTS categories;
 
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     username VARCHAR(255) PRIMARY KEY,
     password VARCHAR(60) NOT NULL
 );
 
+DROP TABLE IF EXISTS majors;
 CREATE TABLE majors (
     major_name VARCHAR(255) PRIMARY KEY
 );
 
+DROP TABLE IF EXISTS projects;
 CREATE TABLE projects (
     project_name VARCHAR(255) UNIQUE PRIMARY KEY,
     project_description TEXT NOT NULL,
     project_image TEXT
 );
 
+DROP TABLE IF EXISTS user_projects;
 CREATE TABLE user_projects (
     project_id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL REFERENCES users(username) ON DELETE CASCADE,
@@ -32,22 +25,26 @@ CREATE TABLE user_projects (
     project_image TEXT
 );
 
+DROP TABLE IF EXISTS user_majors;
 CREATE TABLE user_majors (
     major_id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL REFERENCES users(username) ON DELETE CASCADE,
     major_name VARCHAR(255) NOT NULL REFERENCES majors(major_name) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS categories;
 CREATE TABLE categories (
     category_name VARCHAR(255) PRIMARY KEY
 );
 
+DROP TABLE IF EXISTS user_category;
 CREATE TABLE user_category (
     category_id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL REFERENCES users(username) ON DELETE CASCADE,
     category_name VARCHAR(255) NOT NULL REFERENCES categories(category_name) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
     messages_id SERIAL PRIMARY KEY,
     sender_username VARCHAR(255) REFERENCES users(username) ON DELETE CASCADE,
@@ -56,6 +53,7 @@ CREATE TABLE messages (
     sent_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+DROP TABLE IF EXISTS project_likes;
 CREATE TABLE project_likes (
     likes_id SERIAL PRIMARY KEY,
     project_name VARCHAR(255) REFERENCES projects(project_name) ON DELETE CASCADE,
@@ -63,6 +61,7 @@ CREATE TABLE project_likes (
     UNIQUE (project_name, username)
 );
 
+DROP TABLE IF EXISTS group_messages;
 CREATE TABLE group_messages (
     group_message_id SERIAL PRIMARY KEY,
     group_name VARCHAR(255) REFERENCES categories(category_name) ON DELETE CASCADE,
@@ -71,6 +70,7 @@ CREATE TABLE group_messages (
     sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+DROP TABLE IF EXISTS group_members;
 CREATE TABLE group_members (
     username VARCHAR(255) REFERENCES users(username) ON DELETE CASCADE,
     group_name VARCHAR(255) REFERENCES categories(category_name) ON DELETE CASCADE,
