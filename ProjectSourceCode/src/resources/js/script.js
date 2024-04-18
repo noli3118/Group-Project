e34101acea7fd946bb0ca1f49ce6c49fdc7d10df
Dax <daxlightning1@gmail.com>
2024-04-18 10:43:29 -0600

    projects card template

---
+++ ProjectSourceCode/src/resources/js/script.js
@@ -0,0 +1,18 @@
+fetch("/projects")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((employee) => {
+      // Select the <template> we created in index.html
+      const cardTemplate = document.querySelector('template');
+
+      // Clone a copy of the template we can insert in the DOM as a real visible node
+      const card = cardTemplate.content.cloneNode(true);
+
+      // Update the content of the cloned template with the employee data we queried from the backend
+      card.querySelector('h4').innerText = projects.project_name;
+      card.querySelector('p').innerText = projects.project_description;
+
+      // Append the card as a child with the employee data to the <body> element on our page
+      document.body.appendChild(card);
+    });
+  });
